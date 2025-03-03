81d42032b83366905ccfd9e05691251d900d2883
bb7133 <bb7133@gmail.com>
2025-03-02 21:23:43 -0800

    workloadrepo: make sure WORKLOAD_SCHEMA is ignored by BR (#58878)
    close pingcap/tidb#58768
    
    Signed-off-by: xhe <xw897002528@gmail.com>

--- br/pkg/restore/snap_client/client.go
+++ br/pkg/restore/snap_client/client.go
@@ -52,6 +52,7 @@ import (
 	"github.com/pingcap/tidb/pkg/kv"
 	"github.com/pingcap/tidb/pkg/meta"
 	"github.com/pingcap/tidb/pkg/meta/model"
+	"github.com/pingcap/tidb/pkg/parser/mysql"
 	tidbutil "github.com/pingcap/tidb/pkg/util"
 	"github.com/pingcap/tidb/pkg/util/redact"
 	kvutil "github.com/tikv/client-go/v2/util"
@@ -583,7 +584,7 @@ func (rc *SnapClient) GetDatabases() []*metautil.Database {
 // HasBackedUpSysDB whether we have backed up system tables
 // br backs system tables up since 5.1.0
 func (rc *SnapClient) HasBackedUpSysDB() bool {
-	sysDBs := []string{"mysql", "sys"}
+	sysDBs := []string{mysql.SystemDB, mysql.SysDB, mysql.WorkloadSchema}
 	for _, db := range sysDBs {
 		temporaryDB := utils.TemporaryDBName(db)
 		_, backedUp := rc.databases[temporaryDB.O]
